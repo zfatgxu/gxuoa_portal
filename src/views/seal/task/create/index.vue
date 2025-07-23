ec66f79244973450f3d7aa593bdb5c1cc39c3952
杨宋乐 <13123445458@163.com>
2025-07-23 10:00:05 +0800

    1.修复签字人只能选择一个后，申请单提交失败的问题，是签字人的数据结构变了

--- src/views/seal/task/create/index.vue
+++ src/views/seal/task/create/index.vue
@@ -102,7 +102,8 @@
             <div class="signature-row" v-for="userTask in startUserSelectTasks" :key="userTask.id">
               <span class="required">{{ userTask.name }}</span>
               <el-select
-                v-model="startUserSelectAssignees[userTask.id]"
+                :model-value="getSelectedUserId(userTask.id)"
+                @update:model-value="(value) => handleUserSelect(userTask.id, value)"
                 placeholder="请选择审批人"
                 class="signature-input"
                 filterable
@@ -644,6 +645,17 @@ watch(selectedUnit, (newVal) => {
     getSealTypeOptions()
   }
 })
+
+// 获取选中的审批人ID
+const getSelectedUserId = (taskId) => {
+  const assignees = startUserSelectAssignees.value[taskId]
+  return assignees && assignees.length > 0 ? assignees[0] : ''
+}
+
+// 处理审批人选择变化
+const handleUserSelect = (taskId, value) => {
+  startUserSelectAssignees.value[taskId] = [value]
+}
 </script>
 
 <style scoped>
