f9e089c6dd250fcbdf4be96dd6aa27a1a2d62af6
陈柏兆 <296109644@qq.com>
2025-07-28 11:14:17 +0800

    完善用印申请： 1.创建申请的预览附件使用KKFileView 2.调整页面布局大小（用印材料名过长问题）

--- src/views/seal/task/done/index.vue
+++ src/views/seal/task/done/index.vue
@@ -12,7 +12,7 @@
           </el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="用印材料名" prop="applyData.materialName" width="550" />
+      <el-table-column align="center" label="用印材料名" prop="applyData.materialName" width="350" />
       <el-table-column align="center" label="经办人" width="100">
         <template #default="scope">
           {{ scope.row.applyData.signers ? scope.row.applyData.signers.replace(/,/g, '，').split('，')[0] : '' }}
