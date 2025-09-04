94a606f120a9c3ece51c58287f8c5d28947269c1
wcqqq21 <wcqqq1214@gmail.com>
2025-09-04 15:53:04 +0800

    refactor: 收件人下拉框增加部门显示 refactor: UserVO 添加字段

--- src/api/system/user/index.ts
+++ src/api/system/user/index.ts
@@ -3,18 +3,31 @@ import request from '@/config/axios'
 export interface UserVO {
   id: number
   username: string
+  workId?: string
   nickname: string
-  deptId: number
-  postIds: string[]
+  remark?: string
+  deptIds?: number[]
+  deptNames?: string[]
+  deptNamesStr?: string
+  isChief?: number
+  postIds?: number[]
   email: string
   mobile: string
   sex: number
   avatar: string
-  loginIp: string
   status: number
-  remark: string
+  loginIp: string
   loginDate: Date
   createTime: Date
+  level?: number
+  professionalTitle?: number
+  job?: string
+  idCard?: string
+  deptCode?: string
+  unitSort?: string
+  isRetired?: boolean
+  isBlocked?: boolean
+  isApproved?: boolean
 }
 
 // 查询用户管理列表
@@ -24,7 +37,7 @@ export const getUserPage = (params: PageParam) => {
 
 // 查询用户详情
 export const getUser = (id: number) => {
-  return request.get({ url: '/app/user/get'})
+  return request.get({ url: '/app/user/get', params: { id } })
 }
 
 // 新增用户
